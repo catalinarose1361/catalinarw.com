ba1148142ea05d5380c5372fbbd3d7f70ca0b3a3
Catalina Williams <webdevcatalina@gmail.com>
2021-12-01 17:01:56 -0700

    cleaned up unused code

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,16 @@ import React, { useState } from 'react';
 
 import { Grid, Paper, Container, Switch } from '@mui/material';
 
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import ModeNightIcon from '@mui/icons-material/ModeNight';
-// import Brightness5Icon from '@mui/icons-material/Brightness5';
+
 import Landing from './components/Landing.js';
 
-// import Brightness5Icon from '@material-ui/icons/Brightness5';
-
-// import logo from './logo.svg';
-// import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
 import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
 
-const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
-// import './App.css';
+
+
 
 function App() {
   const [darkMode, setDarkMode] = useState(false)
