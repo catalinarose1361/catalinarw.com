aba3bb7d6b44af0f11c1245694c180a8c792166a
Catalina Williams <webdevcatalina@gmail.com>
2021-12-03 18:26:25 -0700

    added floating button nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,42 @@
 import React, { useState } from 'react';
 
 import { Grid, Paper, Container, Switch } from '@mui/material';
-
-
 import ModeNightIcon from '@mui/icons-material/ModeNight';
-
 import Landing from './components/Landing.js';
-
 import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
-
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-
+import Portfolio from './components/Portfolio';
+import Skills from './components/Skills';
+import Bio from './components/Bio';
+import Documents from './components/Documents'
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
+import ExploreIcon from '@mui/icons-material/Explore';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
+import BrushIcon from '@mui/icons-material/Brush';
+const actions = [
+  { icon: <AccountCircleIcon />, name: 'Biography', href: '#bio' },
+  { icon: <BrushIcon />, name: 'Skills',  href: '#skills' },
+  { icon: <HistoryEduIcon />, name: 'Documents', href: '#documents'},
+  { icon: <MenuBookIcon />, name: 'Portfolio', href: '#portfolio'},
+];
 function App() {
   const [darkMode, setDarkMode] = useState(false)
   const [value, setValue] = React.useState(0);
@@ -50,7 +70,7 @@ function App() {
 
         >
 
-          <Grid
+          {/* <Grid
 
             style={{ textAlign: "center" }}
 
@@ -76,7 +96,7 @@ function App() {
 
             <Brightness4Icon />
 
-          </Grid>
+          </Grid> */}
 
           <Grid
 
@@ -86,9 +106,9 @@ function App() {
 
             sm={12}>
 
-            <Paper id='landing' style={{ height: '500px'}}>
+            <Paper id='bio' style={{ height: '500px'}}>
 
-              <Landing
+              <Bio
 
                 item={true}
 
@@ -106,9 +126,47 @@ function App() {
 
           >
 
-            <Paper style={{ height: '500px' }}>
+            <Paper id='skills' style={{ height: '500px' }}>
 
-              <Landing
+              <Skills
+
+                item={true}
+
+              />
+
+            </Paper>
+           
+          </Grid>
+          <Grid
+
+            style={{ textAlign: "center" }}
+
+            item sm={12}
+
+          >
+
+            <Paper id='documents' style={{ height: '500px' }}>
+
+              <Documents
+
+                item={true}
+
+              />
+
+            </Paper>
+           
+          </Grid>
+          <Grid
+
+            style={{ textAlign: "center" }}
+
+            item sm={12}
+
+          >
+
+            <Paper id='portfolio' style={{ height: '500px' }}>
+
+              <Portfolio
 
                 item={true}
 
@@ -137,17 +195,109 @@ function App() {
         
         >
         <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-          <BottomNavigation
+          {/* <Grid style={{ textAlign: "center" }} */}
+
+            {/* item
+
+            sm={12}> */}
+            <SpeedDial
+              ariaLabel="SpeedDial basic example"
+              sx={{ position: 'absolute', bottom: 65, right: 875 }}
+              icon={<ExploreIcon />}
+            >
+              {actions.map((action) => (
+                <SpeedDialAction
+                  key={action.name}
+                  icon={action.icon}
+                  tooltipTitle={action.name}
+                  href={action.href}
+                />
+              ))}
+            </SpeedDial>
+          {/* </Grid> */}
+          <Grid
+
+            style={{ textAlign: "center" }}
+
+            item
+
+            sm={12}
+
+          >
+            {/* <Fab color="secondary" aria-label="edit">
+              <AddIcon />
+            </Fab> */}
+            {/* <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
+              <SpeedDial
+                ariaLabel="SpeedDial basic example"
+                sx={{ position: 'absolute', bottom: 16, right: 16 }}
+                icon={<SpeedDialIcon />}
+              >
+                {actions.map((action) => (
+                  <SpeedDialAction
+                    key={action.name}
+                    icon={action.icon}
+                    tooltipTitle={action.name}
+                  />
+                ))}
+              </SpeedDial>
+            </Box> */}
+            {/* <ModeNightIcon />
+
+            <Switch
+              id='nightmodeswitch'
+              item={true}
+
+              checked={darkMode}
+
+              onChange={() => setDarkMode(!darkMode)}
+
+              name="checkedA"
+
+            />
+
+            <Brightness4Icon />
+
+          </Grid> */}
+      
+          {/* <BottomNavigation
             showLabels
             value={value}
             onChange={(event, newValue) => {
               setValue(newValue);
             }}
-          >
-            <BottomNavigationAction href='#landing' label="Recents" icon={<RestoreIcon />} />
-            <BottomNavigationAction href='#nightmodeswitch' label="Favorites" icon={<FavoriteIcon />} />
-            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-          </BottomNavigation>
+          > */}
+            {/* <SpeedDial
+              ariaLabel="SpeedDial basic example"
+              sx={{ position: 'absolute', bottom: 16, right: 16 }}
+              icon={<SpeedDialIcon />}
+            >
+              {actions.map((action) => (
+                <SpeedDialAction
+                  key={action.name}
+                  icon={action.icon}
+                  tooltipTitle={action.name}
+                />
+              ))}
+            </SpeedDial> */}
+            {/* <BottomNavigationAction href='#landing' label="Recents" icon={<RestoreIcon />} /> */}
+            <WbSunnyIcon  />
+
+            <Switch
+              id='nightmodeswitch'
+              item={true}
+
+              checked={darkMode}
+
+              onChange={() => setDarkMode(!darkMode)}
+
+              name="checkedA"
+
+            />
+
+            <DarkModeIcon />
+            </Grid>
+       
           </Paper>
         </Box>
      
---
+++ client/src/components/Bio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Bio = () => {
+    return (
+        <div>
+            <h1>
+                Bio
+            </h1>
+        </div>
+    )
+}
+
+export default Bio;
