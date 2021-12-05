50ed23c4241000dd33e945bd3c2bc5949fcd9f35
Catalina Williams <webdevcatalina@gmail.com>
2021-12-05 16:27:16 -0700

    added skill icons

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -6,6 +6,20 @@ const Skills = () => {
             <h1>
                 Skills
             </h1>
+          
+            <img src="https://img.icons8.com/color/40/000000/react-native.png" />
+         
+            <img src="https://img.icons8.com/color/40/000000/material-ui.png" />
+            <img src="https://img.icons8.com/color/40/000000/javascript--v1.png" />
+            <img src="https://img.icons8.com/fluency/40/000000/node-js.png" />
+            <img src="https://img.icons8.com/color/40/000000/html-5--v2.png" />
+            <img src="https://img.icons8.com/color/40/000000/css3.png" />
+            <img src="https://img.icons8.com/color/40/000000/heroku.png" />
+            <img src="https://img.icons8.com/color/40/000000/git.png" />
+            <img src="https://img.icons8.com/color/40/000000/mongodb.png" />
+            <img src="https://img.icons8.com/color/40/000000/graphql.png" />
+            <img src="https://img.icons8.com/color/40/000000/mysql-logo.png" />
+            <img src="https://img.icons8.com/color/40/000000/apollo.png" />
         </div>
     )
 }
