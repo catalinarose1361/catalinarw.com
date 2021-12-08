83a3162a7d846fc465c0d031d5601611786abbf1
Catalina Williams <webdevcatalina@gmail.com>
2021-12-08 13:58:28 -0700

    added document download buttons

--- client/src/components/Documents.js
+++ client/src/components/Documents.js
@@ -1,6 +1,8 @@
 import React, { ReactPDF } from 'react';
 import Resume from '../documents/resume.pdf'
 import Certification from '../documents/certificate.pdf'
+import IconButton from '@mui/material/IconButton';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 const Documents = () => {
     return (
      <>
@@ -10,6 +12,12 @@ const Documents = () => {
             <object data={Certification} type="application/pdf" width="400px" height="500px">
                 <p>Alternative text - include a link <a href={Certification}>to the PDF!</a></p>
             </object>
+            <IconButton download href={Certification}>
+               <FileDownloadIcon />
+            </IconButton>
+            <IconButton download href={Resume}>
+               <FileDownloadIcon />
+            </IconButton>
 </> 
            
     
