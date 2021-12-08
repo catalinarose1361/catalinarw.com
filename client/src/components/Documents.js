c8567bd725e534d72a6a20b135d98a5263124473
Catalina Williams <webdevcatalina@gmail.com>
2021-12-08 14:13:36 -0700

    added grid to portfolio

--- client/src/components/Documents.js
+++ client/src/components/Documents.js
@@ -3,22 +3,41 @@ import Resume from '../documents/resume.pdf'
 import Certification from '../documents/certificate.pdf'
 import IconButton from '@mui/material/IconButton';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 const Documents = () => {
     return (
-     <>
-            <object data={Resume} type="application/pdf" width="400px" height="500px">
-                <p>Alternative text - include a link <a href={Resume}>to the PDF!</a></p>
-            </object>
-            <object data={Certification} type="application/pdf" width="400px" height="500px">
-                <p>Alternative text - include a link <a href={Certification}>to the PDF!</a></p>
-            </object>
+        <Container>
+            <h1>Documents</h1>
+     <Grid container>
+         <Grid xs={6} item>
+                <object data={Resume} type="application/pdf" width="400px" height="500px">
+                    <p>Alternative text - include a link <a href={Resume}>to the PDF!</a></p>
+                </object>
+         </Grid>
+            <Grid xs={6} item >
+                <object data={Certification} type="application/pdf" width="400px" height="500px">
+                    <p>Alternative text - include a link <a href={Certification}>to the PDF!</a></p>
+                </object>
+         </Grid>
+    
+      
+</Grid> 
+     <Grid container>
+         <Grid xs={6} item>
             <IconButton download href={Certification}>
                <FileDownloadIcon />
             </IconButton>
-            <IconButton download href={Resume}>
+         </Grid>
+            <Grid xs={6} item >
+                <IconButton download href={Resume}>
                <FileDownloadIcon />
             </IconButton>
-</> 
+         </Grid>
+    
+     
+</Grid> 
+        </Container>
            
     
     )
